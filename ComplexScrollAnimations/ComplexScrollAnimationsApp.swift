d93cdbe46ef1cf5becd2f88830788a4fadea8672
Elaidzha1940 <eli080293@gmail.com>
2023-11-05 21:14:19 +0300

    ComplexScrollAnimations

---
+++ ComplexScrollAnimations/ComplexScrollAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexScrollAnimationsApp.swift
+//  ComplexScrollAnimations
+//
+//  Created by Elaidzha Shchukin on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ComplexScrollAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComplexScrollAnimations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComplexScrollAnimations
+//
+//  Created by Elaidzha Shchukin on 05.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
