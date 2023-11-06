39103ad072623f72a84b1c2d839c59bf864162fd
Elaidzha1940 <eli080293@gmail.com>
2023-11-06 16:02:28 +0300

    Update MainC + add ExpenseCardView

--- ComplexScrollAnimations/View/MainC.swift
+++ ComplexScrollAnimations/View/MainC.swift
@@ -8,21 +8,54 @@
 //  */
 
 import SwiftUI
-// View Properties
-@State private var allExpense: [Expense] = []
 
 struct MainC: View {
+    // View Properties
+    @State private var allExpense: [Expense] = []
+    
     var body: some View {
         
         ScrollView(.vertical) {
             VStack(spacing: 0){
                 VStack(alignment: .leading, spacing: 15) {
                     Text("Elid.ev")
-                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .font(.system(size: 40, weight: .bold, design: .rounded))
+                        .padding()
+                    
+                    GeometryReader {
+                        let rect = $0.frame(in: .scrollView)
+                    }
+                    .frame(height: 120)
                 }
+                
+                LazyVStack(spacing: 15) {
+                    Menu {
+                        
+                    } label: {
+                        HStack(spacing: 5) {
+                            Text("Filter By")
+                            Image(systemName: "chevron.down")
+                        }
+                        .font(.system(size: 15, weight: .bold, design: .rounded))
+                        .foregroundColor(.gray)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    
+                    ForEach(allExpense) { expense in
+                        ExpenseCardView(expense)
+                    }
+                }
+                .padding(15)
             }
+            .padding(.vertical, 15)
         }
         .scrollIndicators(.hidden)
+    }
+    
+    // Expense Card View
+    @ViewBuilder
+    func ExpenseCardView(_ expense: Expense) -> some View {
+        
     }
 }
 
